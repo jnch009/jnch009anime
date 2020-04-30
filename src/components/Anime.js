3e68ff3326665125eaa73deff3363432a1e49084
jnch009 <jngcheng@sfu.ca>
2020-04-29 22:50:36 -0700

    Cards with title

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,15 +1,32 @@
 import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 import React, { Component, Fragment } from "react";
 
 import Card from "./Card";
 import SearchBar from "./SearchBar";
 
 const title = `Anime for all your needs!`;
-
+const topAnimeToReturn = 4;
 class Anime extends Component {
   constructor() {
     super();
     this.searchInput = React.createRef();
+    this.state = {
+      topAnime: [],
+    };
+  }
+
+  componentDidMount() {
+    const getTopAnime = async () => {
+      const resp = await fetch(`https://api.jikan.moe/v3/top/anime`);
+      const data = await resp.json();
+      const top4 = data.top.slice(0, topAnimeToReturn);
+      console.log(top4);
+      this.setState({
+        topAnime: [...this.state.topAnime, ...top4],
+      });
+    };
+    getTopAnime();
   }
 
   render() {
@@ -17,9 +34,18 @@ class Anime extends Component {
       <Fragment>
         <SearchBar title={title} searchRef={this.searchInput} />
         <Box display="flex" flexDirection="column" alignItems="center">
-          <p>this is a test paragraph</p>
+          <h1>Top Anime!</h1>
         </Box>
-        <Card></Card>
+        <Grid container justify="space-evenly">
+          {this.state.topAnime.map((anime) => (
+            <Card
+              key={anime.mal_id}
+              image={anime.image_url}
+              title={anime.title}
+              startDate={anime.start_date}
+            ></Card>
+          ))}
+        </Grid>
       </Fragment>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,23 @@
 import Box from "@material-ui/core/Box";
 import teal from "@material-ui/core/colors/teal";
+import Typography from "@material-ui/core/Typography";
 import React from "react";
+
+//import "./Card.css";
 
 const color = teal[500];
 
-const Card = () => {
+const Card = ({ image, title, startDate }) => {
+  console.log(image);
   return (
-    <Box bgcolor={color}>
-      <p>testing123</p>
+    <Box bgcolor={color} m={3} p={7}>
+      <img className="card" width={250} height={400} src={image} alt="" />
+      <Typography align="center">
+        <h4>
+          <strong>{title}</strong>
+        </h4>
+        <h6>Debut: {startDate}</h6>
+      </Typography>
     </Box>
   );
 };
