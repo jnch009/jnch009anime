d294ab98d834d2d89da79958deb86b2e93c5f0ae
jnch009 <jngcheng@sfu.ca>
2020-04-29 20:01:39 -0700

    Passing title as props

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,12 +1,16 @@
 import Box from "@material-ui/core/Box";
 import React, { Component, Fragment } from "react";
 
+import SearchBar from "./SearchBar";
+
+const title = `Anime for all your needs!`;
+
 class Anime extends Component {
   render() {
     return (
       <Fragment>
+        <SearchBar title={title} />
         <Box display="flex" flexDirection="column" alignItems="center">
-          <h1>Anime for all your needs!</h1>
           <p>this is a test paragraph</p>
         </Box>
       </Fragment>
