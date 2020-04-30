786d35595becb93f46006e37b9f7cc82c0ce95a3
jnch009 <jngcheng@sfu.ca>
2020-04-30 00:12:32 -0700

    Transition on hover of image

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,34 @@
 import Box from "@material-ui/core/Box";
 import teal from "@material-ui/core/colors/teal";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, { useState } from "react";
 
 import "./Card.css";
 
 const color = teal[500];
 
 const Card = ({ image, title, startDate }) => {
-  console.log(image);
+  //console.log(image);
+  const [hoverDetails, setHoverDetails] = useState(false);
+
   return (
     <Box bgcolor={color} m={3} p={7}>
-      <div class="image">
-        <img className="card" width={250} height={400} src={image} alt="" />
-        <p>Click for more details</p>
-      </div>
+      <Box
+        className={hoverDetails ? "image hvr-fade" : null}
+        onPointerEnter={() => setHoverDetails(true)}
+        onPointerLeave={() => setHoverDetails(false)}
+      >
+        <img
+          className={hoverDetails ? "imgOpacity" : null}
+          width={250}
+          height={400}
+          src={image}
+          alt=""
+        />
+        <p className={hoverDetails ? null : "hidden"}>Click for more details</p>
+      </Box>
       <Typography align="center">
-        <h4>
+        <h4 className="hvr-fade">
           <strong>{title}</strong>
         </h4>
         <h6>Debut: {startDate}</h6>
