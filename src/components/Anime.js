1ff7d094ba0f123c130a71e762bb71d63045a66a
jnch009 <jngcheng@sfu.ca>
2020-04-30 18:10:13 -0700

    eslint fixes

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,10 +1,10 @@
-import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Pagination from "@material-ui/lab/Pagination";
-import React, { Component, Fragment } from "react";
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Pagination from '@material-ui/lab/Pagination';
+import React, { Component } from 'react';
 
-import Card from "./Card";
-import SearchBar from "./SearchBar";
+import Card from './Card';
+import SearchBar from './SearchBar';
 
 const title = `Anime for all your needs!`;
 const topAnimeToReturn = 5;
@@ -20,33 +20,14 @@ class Anime extends Component {
     };
   }
 
-  fetchNewPage(page) {
-    this.setState(
-      {
-        currentPage: page,
-      },
-      async () => {
-        const offset = this.state.currentPage * topAnimeToReturn - 5;
-        const resp = await fetch(`https://api.jikan.moe/v3/top/anime`);
-        const data = await resp.json();
-        const top5 = data.top.slice(offset, offset + topAnimeToReturn);
-        this.setState({
-          topAnime: [...top5],
-        });
-      }
-    );
-  }
-
   componentDidMount() {
     const getTopAnime = async () => {
+      const { offset } = this.state;
       const resp = await fetch(`https://api.jikan.moe/v3/top/anime`);
       const data = await resp.json();
       console.log(data.top.slice());
       console.log(data.top.length / topAnimeToReturn);
-      const top5 = data.top.slice(
-        this.state.offset,
-        this.state.offset + topAnimeToReturn
-      );
+      const top5 = data.top.slice(offset, offset + topAnimeToReturn);
       this.setState({
         topAnime: [...top5],
         totalPages: data.top.length / topAnimeToReturn,
@@ -55,29 +36,48 @@ class Anime extends Component {
     getTopAnime();
   }
 
+  fetchNewPage(page) {
+    this.setState(
+      {
+        currentPage: page,
+      },
+      async () => {
+        const { currentPage } = this.state;
+        const offset = currentPage * topAnimeToReturn - 5;
+        const resp = await fetch(`https://api.jikan.moe/v3/top/anime`);
+        const data = await resp.json();
+        const top5 = data.top.slice(offset, offset + topAnimeToReturn);
+        this.setState({
+          topAnime: [...top5],
+        });
+      },
+    );
+  }
+
   render() {
+    const { topAnime, totalPages, currentPage } = this.state;
     return (
-      <Fragment>
+      <>
         <SearchBar title={title} searchRef={this.searchInput} />
-        <Box display="flex" flexDirection="column" alignItems="center">
+        <Box display='flex' flexDirection='column' alignItems='center'>
           <h1>Top Anime!</h1>
         </Box>
-        <Grid container justify="space-evenly">
-          {this.state.topAnime.map((anime) => (
+        <Grid container justify='space-evenly'>
+          {topAnime.map((anime) => (
             <Card
               key={anime.mal_id}
               image={anime.image_url}
               title={anime.title}
               startDate={anime.start_date}
-            ></Card>
+            />
           ))}
         </Grid>
         <Pagination
-          count={this.state.totalPages}
-          page={this.state.currentPage}
+          count={totalPages}
+          page={currentPage}
           onChange={(e, page) => this.fetchNewPage(page)}
-        ></Pagination>
-      </Fragment>
+        />
+      </>
     );
   }
 }
