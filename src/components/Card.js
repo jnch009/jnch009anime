fd4bb73e676748e930457c2f5a5b87c5d7a8d8a4
jnch009 <jngcheng@sfu.ca>
2020-04-30 22:43:19 -0700

    Modal template

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,7 @@ import Box from '@material-ui/core/Box';
 import teal from '@material-ui/core/colors/teal';
 import Typography from '@material-ui/core/Typography';
 import React, { useState } from 'react';
+import Details from './Details';
 
 import './Card.css';
 
@@ -9,13 +10,16 @@ const color = teal[500];
 
 const Card = ({ image, title, startDate }) => {
   const [hoverDetails, setHoverDetails] = useState(false);
+  const [modal, setModal] = useState(false);
 
   return (
     <Box bgcolor={color} m={3} p={7}>
+      <Details openModal={modal} setModal={setModal} />
       <Box
         className={hoverDetails ? 'image hvr-fade' : null}
         onPointerEnter={() => setHoverDetails(true)}
         onPointerLeave={() => setHoverDetails(false)}
+        onClick={() => setModal(true)}
       >
         <img
           className={hoverDetails ? 'imgOpacity' : null}
