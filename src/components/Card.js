f7383e588737fc1f5af7467b70af309e19393a1b
jnch009 <jngcheng@sfu.ca>
2020-05-01 15:57:14 -0700

    Card hover effect

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,27 +2,30 @@ import Box from '@material-ui/core/Box';
 import teal from '@material-ui/core/colors/teal';
 import Typography from '@material-ui/core/Typography';
 import React, { useState } from 'react';
+
 import Details from './Details';
 
 import './Card.css';
 
 const color = teal[500];
 
-const Card = ({ image, title, startDate }) => {
+const Card = ({ id, image, title, startDate }) => {
   const [hoverDetails, setHoverDetails] = useState(false);
   const [modal, setModal] = useState(false);
 
   return (
     <Box bgcolor={color} m={3} p={7}>
-      <Details openModal={modal} setModal={setModal} />
+      <Details openModal={modal} setModal={setModal} id={id} />
       <Box
         className={hoverDetails ? 'image hvr-fade' : null}
-        onPointerEnter={() => setHoverDetails(true)}
+        onPointerEnter={() => {
+          setHoverDetails(true);
+        }}
         onPointerLeave={() => setHoverDetails(false)}
         onClick={() => setModal(true)}
       >
         <img
-          className={hoverDetails ? 'imgOpacity' : null}
+          className={hoverDetails ? 'imgOpacity' : 'imgHover'}
           width={250}
           height={400}
           src={image}
